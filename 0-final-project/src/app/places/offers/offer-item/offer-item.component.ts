7ba24ec3d7d3e779098c2e4d1d6f7cb874e942a5
Elnisky <leonardoelnisky@hotmail.com>
2025-04-15 14:31:33 -0300

    Section 09: Styling & Theming Ionic Apps

---
+++ 0-final-project/src/app/places/offers/offer-item/offer-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+import { Place } from '../../place.model';
+
+@Component({
+  selector: 'app-offer-item',
+  templateUrl: './offer-item.component.html',
+  styleUrls: ['./offer-item.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    RouterModule
+  ]
+})
+export class OfferItemComponent  implements OnInit {
+  @Input() offer!: Place;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  getDummyDate() {
+    let today = new Date();
+
+    let month = String(today.getMonth() + 1).padStart(2, '0'); // getMonth() is 0-based
+    let day = String(today.getDate()).padStart(2, '0');
+    let year = today.getFullYear();
+
+    let formatted = `${month}-${day}-${year}`;
+    return formatted;
+  }
+}
